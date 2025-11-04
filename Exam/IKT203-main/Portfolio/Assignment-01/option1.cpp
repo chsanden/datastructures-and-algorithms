729d69a399898ee7772e94b1c0146aa6c6f603e1
Christopher Sanden <christops@uia.no>
2025-11-04 18:36:16 +0100

    Renaming and retructuring

--- Exam/IKT203-main/Portfolio/Assignment-01/option1.cpp
+++
@@ -1,9 +0,0 @@
-// Option 1 (Standard): Console Text Editor.
-//
-
-#include "option1.h"
-
-int RunApp() {
-	// Implement the Console Text Editor application logic here
-	return 0;
-}
--- Exam/IKT203-main/Portfolio/Assignment-03/option2.cpp
+++
@@ -1,6 +0,0 @@
-#include "option2.h"
-
-int RunApp() {
-	// Implement the Console Music Player application logic here
-	return 0;
-}
