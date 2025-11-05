8b5f7537731f3f2b313d219a53e78faeb6d7b13a
Christopher Sanden <christops@uia.no>
2025-11-05 23:32:52 +0100

    cleaned up excess projects

--- SimpleSearch/FCompareAccount.h
+++
@@ -1,8 +0,0 @@
-#ifndef SIMPLESEARCH_FCOMPAREACCOUNT_H
-#define SIMPLESEARCH_FCOMPAREACCOUNT_H
-#include "TBankAccount.h"
-
-using FCompareAccount = bool (*)(TBankAccount* account, void* searchKey);
-
-
-#endif //SIMPLESEARCH_FCOMPAREACCOUNT_H
--- SimpleSearch/SearchSummary.h
+++
@@ -1,9 +0,0 @@
-#ifndef SIMPLESEARCH_SEARCHSUMMARY_H
-#define SIMPLESEARCH_SEARCHSUMMARY_H
-
-struct SearchSummary {
-    long long comparisons = 0;
-    double timeSpentMs = 0;
-};
-
-#endif //SIMPLESEARCH_SEARCHSUMMARY_H
